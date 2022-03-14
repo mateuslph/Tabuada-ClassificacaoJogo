10d3aa64112b675c084da29c5483ba69b1d77f07
mateuslph <mlptche@gmail.com>
2022-03-14 20:17:55 -0300

    resolvido problema

---
+++ Classificacao.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Classificacao {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int golsNossoPartida, golsNossoTotal, golsSofridoPartida, golsSofridoTotal, partida, nossosPontos;
+
+        nossosPontos = 0;
+        golsNossoTotal = 0;
+        golsSofridoTotal = 0;
+
+        for (partida = 1; partida <= 5; partida++) {
+            System.out.println("Informe o número de gols do nosso time na partida " + partida + ":");
+            golsNossoPartida = sc.nextInt();
+            golsNossoTotal += golsNossoPartida;
+
+            System.out.println("Informe o número de gols do time adversário na partida " + partida + ":");
+            golsSofridoPartida = sc.nextInt();
+            golsSofridoTotal += golsSofridoPartida;
+
+            if (golsNossoPartida == golsSofridoPartida) {
+                nossosPontos += 1;
+                continue;
+            }
+
+            if (golsNossoPartida > golsSofridoPartida) {
+                nossosPontos += 3;
+                continue;
+            }
+
+            sc.close();
+
+        }
+
+        System.out.println("A pontuação total do nosso time no campeonato é: " + nossosPontos);
+
+        if (nossosPontos >= 10) {
+            System.out.println("Nosso time está classificado para o mata-mata!");
+
+        }
+
+        System.out.println("Nosso time marcou: " + golsNossoTotal + " gols");
+        System.out.println("Nosso time sofreu: " + golsSofridoTotal + " gols");
+
+    }
+
+}
---
+++ Tabuada.java
@@ -0,0 +1,15 @@
+public class Tabuada {
+    public static void main(String[] arg) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i*j + " ");
+
+            }
+            
+            System.out.println();
+
+        }
+
+    }
+
+}
