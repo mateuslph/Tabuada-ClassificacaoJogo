ad4d2597aa5a6d4406e41b58639d84a77f003d4d
mateuslph <mlptche@gmail.com>
2022-03-19 15:53:36 -0300

    add identacao em todos e try em Classificacao

--- Classificacao.java
+++ Classificacao.java
@@ -3,45 +3,50 @@ import java.util.Scanner;
 public class Classificacao {
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
-        int golsNossoPartida, golsNossoTotal, golsSofridoPartida, golsSofridoTotal, partida, nossosPontos;
+        try (Scanner sc = new Scanner(System.in)) {
+            
+            int golsNossoPartida, golsNossoTotal, golsSofridoPartida, golsSofridoTotal, partida, nossosPontos;
 
-        nossosPontos = 0;
-        golsNossoTotal = 0;
-        golsSofridoTotal = 0;
+            nossosPontos = 0;
+            golsNossoTotal = 0;
+            golsSofridoTotal = 0;
 
-        for (partida = 1; partida <= 5; partida++) {
-            System.out.println("Informe o número de gols do nosso time na partida " + partida + ":");
-            golsNossoPartida = sc.nextInt();
-            golsNossoTotal += golsNossoPartida;
+            for (partida = 1; partida <= 5; partida++) {
+                System.out.println("Informe o número de gols do nosso time na partida " + partida + ":");
+                golsNossoPartida = sc.nextInt();
+                golsNossoTotal += golsNossoPartida;
 
-            System.out.println("Informe o número de gols do time adversário na partida " + partida + ":");
-            golsSofridoPartida = sc.nextInt();
-            golsSofridoTotal += golsSofridoPartida;
+                System.out.println("Informe o número de gols do time adversário na partida " + partida + ":");
+                golsSofridoPartida = sc.nextInt();
+                golsSofridoTotal += golsSofridoPartida;
 
-            if (golsNossoPartida == golsSofridoPartida) {
-                nossosPontos += 1;
-                continue;
+                if (golsNossoPartida == golsSofridoPartida) {
+                    nossosPontos += 1;
+                    continue;
+                }
+
+                if (golsNossoPartida > golsSofridoPartida) {
+                    nossosPontos += 3;
+                    continue;
+                }
+
             }
 
-            if (golsNossoPartida > golsSofridoPartida) {
-                nossosPontos += 3;
-                continue;
+            System.out.println("A pontuação total do nosso time no campeonato é: " + nossosPontos);
+
+            if (nossosPontos >= 10) {
+                System.out.println("Nosso time está classificado para o mata-mata!");
+
+            } else {
+                System.out.println("Nosso time foi desclassificado");
+
             }
 
+            System.out.println("Nosso time marcou: " + golsNossoTotal + " gols");
+            System.out.println("Nosso time sofreu: " + golsSofridoTotal + " gols");
+
+            sc.close();
         }
-
-        System.out.println("A pontuação total do nosso time no campeonato é: " + nossosPontos);
-
-        if (nossosPontos >= 10) {
-            System.out.println("Nosso time está classificado para o mata-mata!");
-
-        }
-
-        System.out.println("Nosso time marcou: " + golsNossoTotal + " gols");
-        System.out.println("Nosso time sofreu: " + golsSofridoTotal + " gols");
-
-        sc.close();
 
     }
 
--- Tabuada.java
+++ Tabuada.java
@@ -1,7 +1,11 @@
 public class Tabuada {
+    
     public static void main(String[] arg) {
+
         for (int i = 1; i <= 9; i++) {
+
             for (int j = 1; j <= 10; j++) {
+
                 System.out.print(i*j + " ");
 
             }
