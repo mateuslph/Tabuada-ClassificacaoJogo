ecee28a357e0c2d97cb9cef2a8fd578e82ded338
mateuslph <mlptche@gmail.com>
2022-03-21 21:32:57 -0300

    corrigido identacao

--- Classificacao.java
+++ Classificacao.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Classificacao {
+
     public static void main(String[] args) {
         
         try (Scanner sc = new Scanner(System.in)) {
@@ -12,6 +13,7 @@ public class Classificacao {
             golsSofridoTotal = 0;
 
             for (partida = 1; partida <= 5; partida++) {
+
                 System.out.println("Informe o número de gols do nosso time na partida " + partida + ":");
                 golsNossoPartida = sc.nextInt();
                 golsNossoTotal += golsNossoPartida;
@@ -21,13 +23,17 @@ public class Classificacao {
                 golsSofridoTotal += golsSofridoPartida;
 
                 if (golsNossoPartida == golsSofridoPartida) {
+
                     nossosPontos += 1;
                     continue;
+
                 }
 
                 if (golsNossoPartida > golsSofridoPartida) {
+
                     nossosPontos += 3;
                     continue;
+
                 }
 
             }
@@ -35,9 +41,11 @@ public class Classificacao {
             System.out.println("A pontuação total do nosso time no campeonato é: " + nossosPontos);
 
             if (nossosPontos >= 10) {
+
                 System.out.println("Nosso time está classificado para o mata-mata!");
 
             } else {
+
                 System.out.println("Nosso time foi desclassificado");
 
             }
@@ -46,6 +54,7 @@ public class Classificacao {
             System.out.println("Nosso time sofreu: " + golsSofridoTotal + " gols");
 
             sc.close();
+            
         }
 
     }
